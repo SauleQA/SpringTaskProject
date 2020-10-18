f29836192fd8844f57804dc273f040a0819cca59
STussupbekova <sauletuss@gmail.com>
2020-10-18 18:02:43 +0600

    first commit

---
+++ src/main/kotlin/com/sauletest/testapi/TestapiApplication.kt
@@ -0,0 +1,11 @@
+package com.sauletest.testapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<TestapiApplication>(*args)
+}
---
+++ src/test/kotlin/com/sauletest/testapi/TestapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sauletest.testapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
